0ffef8adcf6cca75a2cfdae48abfc0d0a272acdf
Ismael <fs.ismael@gmail.com>
2021-03-10 17:21:56 -0300

    Adding source Files

---
+++ TrayMonitor/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+using System.Management;
+
+namespace TrayMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "CPU Speed";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "CPU Speed";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+            //
+            // MaxCpu
+            //
+            maxCPU = getMaxCPU();
+
+        }
+       
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Timer timer1;
+        private int maxCPU;
+        
+        private int getMaxCPU()
+        {
+            ManagementObjectSearcher objSearchCPU = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM  Win32_Processor");
+            ManagementObjectCollection.ManagementObjectEnumerator cpuEnum = objSearchCPU.Get().GetEnumerator();
+            cpuEnum.MoveNext();
+            return System.Int32.Parse(cpuEnum.Current["MaxClockSpeed"].ToString());
+        }
+    }
+}
+
---
+++ TrayMonitor/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrayMonitor
+{/*
+  * Developer references:
+  * https://stackoverflow.com/questions/36379547/writing-text-to-the-system-tray-instead-of-an-icon
+  * https://stackoverflow.com/questions/1364809/the-windows-gui-main-loop-in-c-where-is-it
+  * https://stackoverflow.com/questions/6944779/determine-operating-system-and-processor-type-in-c-sharp
+  * https://stackoverflow.com/questions/1730731/how-to-start-winform-app-minimized-to-tray
+  * https://www.codeproject.com/Articles/21483/Create-Icons-at-Runtime-and-Show-Them-in-the-Syste
+  * https://www.codeproject.com/Articles/26310/Using-WMI-to-retrieve-processor-information-in-C
+  * http://www.tutorialspanel.com/create-system-tray-icon-windows-forms-application-using-c-vb-net/index.htm
+  * https://docs.microsoft.com/pt-br/dotnet/csharp/programming-guide/inside-a-program/coding-conventions
+  * https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-processor
+  * https://docs.microsoft.com/en-us/windows/win32/wmisdk/retrieving-an-instance
+  * 
+  */
+    public partial class Form1 : Form
+    {
+        private String formattedCPUSpeed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void SetVisibleCore(bool value)
+        {
+            value = false;
+            if (!this.IsHandleCreated) 
+                CreateHandle();
+            
+            base.SetVisibleCore(value);
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Application.Exit();
+        }
+        public void CreateTextIcon(string str)
+        {
+            //Font fontToUse = new Font("Microsoft Sans Serif", 11, FontStyle.Regular, GraphicsUnit.Pixel);
+            //Font fontToUse = new Font("Tahoma", 11, FontStyle.Regular, GraphicsUnit.Pixel);
+            Font fontToUse = new Font("Trebuchet MS", 11, FontStyle.Regular, GraphicsUnit.Pixel);
+            Brush brushToUse = new SolidBrush(Color.Black);
+            Bitmap bitmapText = new Bitmap(16, 16);
+            Graphics g = System.Drawing.Graphics.FromImage(bitmapText);
+
+            IntPtr hIcon;
+
+            g.Clear(Color.Yellow);
+            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.SingleBitPerPixelGridFit;
+            g.DrawString(str, fontToUse, brushToUse, -2, 0);
+            hIcon = (bitmapText.GetHicon());
+            notifyIcon1.Icon = System.Drawing.Icon.FromHandle(hIcon);
+            
+            g.Dispose();
+            bitmapText.Dispose();
+            brushToUse.Dispose();
+            fontToUse.Dispose();
+            hIcon = IntPtr.Zero;
+        }
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(formattedCPUSpeed);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            notifyIcon1.BalloonTipIcon = ToolTipIcon.Info;
+            notifyIcon1.BalloonTipText = "Você pode conferir a velocidade do seu processador no ícone ao lado do relógio";
+            notifyIcon1.BalloonTipTitle = "Velocidade do Processador";
+            notifyIcon1.ShowBalloonTip(2000);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            formattedCPUSpeed = getCPUSpeed1().ToString("N1");
+            notifyIcon1.Text = formattedCPUSpeed + " Ghz";
+            CreateTextIcon(formattedCPUSpeed);
+        }
+
+        private Double getCPUSpeed1()
+        {
+            /*
+            * Implemented WMI access
+            * https://docs.microsoft.com/en-us/windows/win32/wmisdk/retrieving-an-instance
+            * https://docs.microsoft.com/en-us/windows/win32/cimwin32prov/win32-processor
+            * https://stackoverflow.com/questions/1730731/how-to-start-winform-app-minimized-to-tray
+            */
+
+            ManagementObjectSearcher objSearchPerf = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM  Win32_PerfFormattedData_Counters_ProcessorInformation");
+            ManagementObjectCollection colPerf = objSearchPerf.Get();
+            Double cpuPerf = 0;
+
+            foreach (ManagementObject objPerf in colPerf)
+            {
+                Console.WriteLine("CPU Performance: ", objPerf["PercentProcessorPerformance"]);
+                cpuPerf += Double.Parse(objPerf["PercentProcessorPerformance"].ToString());
+            }
+            return maxCPU * (cpuPerf/(100000 * colPerf.Count));
+        }
+    }
+}
